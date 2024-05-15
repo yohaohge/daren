05b97bc32891f0d6fb3ff70dd2a60c2feaf27b05
zhangou <zhangou@wepie.com>
2024-05-15 14:45:32 +0800

    窗口

--- didadog/__init__.py
+++ didadog/__init__.py
@@ -6,8 +6,12 @@ from selenium.webdriver.common.by import By
 
 # 启动程序， 打开登录页面
 
-option = webdriver.ChromeOptions()
-option.add_argument("--user-data-dir="+r"/Users/ouge/Library/Application Support/Google/Chrome/Profile 3/")
-driver = webdriver.Chrome(options=option)   # 打开chrome浏览器
+class WebManager:
+    def __init__(self):
+        option = webdriver.ChromeOptions()
+        option.add_argument("--user-data-dir="+r"/Users/ouge/Library/Application Support/Google/Chrome/Profile 3/")
+        driver = webdriver.Chrome(options=option)   # 打开chrome浏览器
+        time.sleep(200)
 
-time.sleep(200)
+def LoadWeb():
+    webManger = WebManager()
